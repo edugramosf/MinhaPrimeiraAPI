5bd20c9757fa664f1f3fd87f77a04ffc97bb14ca
Eduardo Ramos <edugramosf@gmail.com>
2023-02-09 23:46:31 -0300

    Atualização 0.3 - endpoint /AddHeader

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World 4!");
-app.MapPost("/user", () => "Peter Parker");
+app.MapPost("/", () => new { Name = "Peter Parker", Age = 35 });
+app.MapGet("/AddHeader", (HttpResponse response) => {
+    response.Headers.Add("Teste", "Peter Parker");
+    return new { Name = "Peter Parker", Age = 35 };
+    });
 
 app.Run();
