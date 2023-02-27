8ee8fd5c0d14b0a029646410a73613cc61bee87e
Eduardo <edugramosf@gmail.com>
2023-02-27 10:38:30 -0300

    Atualização 1.1 - Arquivo de Configuração

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapPost("/products", (Product product) =>
 {
@@ -42,12 +44,16 @@ app.Run();
 //static para continuar funcionando após cada requisição
 public static class ProductRepository
 {
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
+
+    public static void Init(IConfiguration configuration)
+    {
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
 
     public static void Add(Product product) 
-    {
-        if (Products == null) { Products = new List<Product>(); }
-                           
+    {                   
         Products.Add(product);
     }
         
