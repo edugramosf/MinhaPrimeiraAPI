cd7b522cb00a38eecb65ffd8eadd005776e43313
Eduardo <edugramosf@gmail.com>
2023-03-01 13:32:30 -0300

    Atualização 1.3 - Adicionando DbContext como serviço

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>();
+
+
 var app = builder.Build();
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
@@ -74,4 +78,12 @@ public static class ProductRepository
 public class Product
 {
     public string Code { get; set; }  public string Name { get; set; }
+}
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=@Sql2019;MultipleActiveResultsSets=true;Encrypt=YES;TrustServerCertificate=YES");
 }
