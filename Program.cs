29bc1fb759edca73541ec26bb219d1d6969c5176
Eduardo <edugramosf@gmail.com>
2023-03-01 17:36:59 -0300

    Atualização 1.4 - Migrations

--- Program.cs
+++ Program.cs
@@ -77,6 +77,8 @@ public static class ProductRepository
 
 public class Product
 {
+    public int Id { get; set; } 
+    
     public string Code { get; set; }  public string Name { get; set; }
 }
 
